d7fc2cfa6ab04da7ea9b149e8f03c74ab30b8fe7
Sofia "dzshn" N. L <zshn@pm.me>
2021-12-25 02:38:42 -0300

    refactor: make engine a submodule

---
+++ tetris/engine/abcs.py
@@ -0,0 +1,50 @@
+import abc
+from collections.abc import Iterator
+
+from tetris.types import Board
+from tetris.types import DeltaFrame
+from tetris.types import KickTable
+from tetris.types import Minos
+from tetris.types import PieceType
+from tetris.types import QueueSeq
+from tetris.types import Seed
+
+
+class RotationSystem(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def shapes(self) -> dict[PieceType, list[Minos]]:
+        ...
+
+    @property
+    @abc.abstractmethod
+    def kicks(self) -> KickTable:
+        ...
+
+
+class Queue(abc.ABC):
+    def __init__(self, seed: Seed):
+        ...
+
+    @property
+    @abc.abstractmethod
+    def pieces(self) -> QueueSeq:
+        ...
+
+    @property
+    @abc.abstractmethod
+    def bag(self) -> QueueSeq:
+        ...
+
+    @abc.abstractmethod
+    def pop(self) -> PieceType:
+        ...
+
+    def __iter__(self) -> Iterator[PieceType]:
+        yield from self.pieces
+
+
+class Scorer(abc.ABC):
+    @abc.abstractmethod
+    def judge(self, board: Board, delta: DeltaFrame) -> int:
+        ...
---
+++ tetris/engine/queue.py
@@ -0,0 +1,38 @@
+import random
+
+from tetris.engine.abcs import Queue
+from tetris.types import PieceType
+from tetris.types import QueueSeq
+from tetris.types import Seed
+
+
+class SevenBag(Queue):
+    def __init__(self, seed: Seed, queue: list[int] = [], bag: list[int] = []):
+        self._random = random.Random(seed)
+        self._queue = [PieceType(i) for i in queue]
+        self._bag = [PieceType(i) for i in bag]
+        while len(self._queue) < 4:
+            self._queue.append(self._next_piece())
+
+    def _next_piece(self) -> PieceType:
+        if not self._bag:
+            self._bag = list(PieceType)
+            self._random.shuffle(self._bag)
+
+        return self._bag.pop()
+
+    @property
+    def pieces(self) -> QueueSeq:
+        return self._queue.copy()
+
+    @property
+    def bag(self) -> QueueSeq:
+        return self._bag.copy()
+
+    def pop(self) -> PieceType:
+        piece = self._queue.pop(0)
+        self._queue.append(self._next_piece())
+        return piece
+
+    def __repr__(self) -> str:
+        return f"SevenBag(queue={self.pieces}, bag={self.bag})"
