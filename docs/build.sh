217786ad1e7d3ea915f9a7bf799d8265e860e0ec
Sofia "dzshn" N. L <zshn@pm.me>
2022-02-21 01:50:42 -0300

    docs: theme pdoc pages

--- docs/build.sh
+++ docs/build.sh
@@ -4,4 +4,13 @@ PATH_DOCS=$(realpath $(dirname $0))
 PATH_PROJ=$(realpath $PATH_DOCS/..)
 
 rm -r $PATH_DOCS/build
-pdoc -o $PATH_DOCS/build -d numpy $PATH_PROJ/tetris
+mkdir $PATH_DOCS/build
+
+cp $PATH_DOCS/logo.png $PATH_DOCS/build
+cp $PATH_DOCS/favicon.ico $PATH_DOCS/build
+
+pdoc -o $PATH_DOCS/build \
+     -t $PATH_DOCS \
+     -d numpy \
+     --logo /logo.png \
+     $PATH_PROJ/tetris
