c7afe6d1c505e70d2147de2aabd453715f65cef8
Sofia N. Lima <me@dzshn.xyz>
2022-10-12 09:24:49 -0300

    refactors and `__setitem__` slices

--- tetris/board.py
+++ tetris/board.py
@@ -16,43 +16,6 @@ _OUT_OF_BOUNDS = "board index {} out of bounds for axis {}"
 _BAD_VALUE_TYPE = "board values must be integers"
 
 
-def _normalize_index(
-    shape: tuple[int, ...], key: Union[int, slice, tuple[int, int]]
-) -> Union[int, slice, tuple[int, int]]:
-    """Apply various checks and convert things like negative indices."""
-    if hasattr(key, "__index__"):
-        k = int(key)  # type: ignore[arg-type]
-        if k < 0:
-            k += shape[0]
-        if not 0 <= k < shape[0]:
-            raise IndexError(_OUT_OF_BOUNDS.format(key, 0))
-        return k
-
-    if isinstance(key, slice):
-        return key
-
-    if isinstance(key, tuple):
-        if len(shape) != 2 or len(key) != 2:
-            raise ValueError("can only index 2d arrays with 2-tuples")
-        a, b = key
-        if not hasattr(a, "__index__") or not hasattr(b, "__index__"):
-            raise TypeError(
-                _BAD_INDEX_TYPE.format(f"({type(a).__name__}, {type(b).__name__})")
-            )
-        a, b = int(a), int(b)
-        if a < 0:
-            a += shape[0]
-        if not 0 <= a < shape[0]:
-            raise IndexError(_OUT_OF_BOUNDS.format(key[0], 0))
-        if b < 0:
-            b += shape[1]
-        if not 0 <= b < shape[1]:
-            raise IndexError(_OUT_OF_BOUNDS.format(key[1], 1))
-        return a, b
-
-    raise TypeError(_BAD_INDEX_TYPE.format(type(k).__name__))
-
-
 class Board:
     """2-dimensional array object."""
 
@@ -151,10 +114,58 @@ class Board:
             + self._strides[0] * self._shape[0] : self._strides[0]
         ].tobytes()
 
+    def _normalize_index(
+        self, key: Union[int, slice, tuple[int, int]]
+    ) -> Union[int, slice, tuple[int, int]]:
+        """Apply various checks and convert things like negative indices."""
+        if hasattr(key, "__index__"):
+            k = int(key)  # type: ignore[arg-type]
+            if k < 0:
+                k += self._shape[0]
+            if not 0 <= k < self._shape[0]:
+                raise IndexError(_OUT_OF_BOUNDS.format(key, 0))
+            return k
+
+        if isinstance(key, slice):
+            return key
+
+        if isinstance(key, tuple):
+            if self._ndim != 2 or len(key) != 2:
+                raise ValueError("can only index 2d arrays with 2-tuples")
+            a, b = key
+            if not hasattr(a, "__index__") or not hasattr(b, "__index__"):
+                raise TypeError(
+                    _BAD_INDEX_TYPE.format(f"({type(a).__name__}, {type(b).__name__})")
+                )
+            a, b = int(a), int(b)
+            if a < 0:
+                a += self._shape[0]
+            if not 0 <= a < self._shape[0]:
+                raise IndexError(_OUT_OF_BOUNDS.format(key[0], 0))
+            if b < 0:
+                b += self._shape[1]
+            if not 0 <= b < self._shape[1]:
+                raise IndexError(_OUT_OF_BOUNDS.format(key[1], 1))
+            return a, b
+
+        raise TypeError(_BAD_INDEX_TYPE.format(type(k).__name__))
+
+    def _contiguous_blocks(self, key: slice):
+        """Yield largest contiguous blocks for a given slice."""
+        start, stop, step = key.indices(self._shape[0])
+        stride = self._strides[0] * step
+        offset = self._offset + start * self._strides[0]
+        length = stride * max(math.ceil((stop - start) / step), 0)
+        if self._ndim == 1:
+            yield (offset, offset + length, stride)
+        else:
+            for i in range(offset, offset + length, stride):
+                yield (i, i + self._strides[1] * self._shape[1], self._strides[1])
+
     def __getitem__(self, key: Union[int, slice, tuple[int, int]]) -> Union[Board, int]:
         # TODO: tuples with slices? might be unnecessary
 
-        key = _normalize_index(self._shape, key)
+        key = self._normalize_index(key)
 
         if isinstance(key, int):
             if self._ndim == 1:
@@ -186,28 +197,33 @@ class Board:
     def __setitem__(
         self, key: Union[int, slice, tuple[int, int]], value: Union[Board, int]
     ) -> None:
-        key = _normalize_index(self._shape, key)
+        key = self._normalize_index(key)
 
         if isinstance(key, int):
-            if self._ndim == 1:
-                if hasattr(value, "__len__"):
+            if hasattr(value, "__len__"):
+                if self._ndim == 1:
                     raise TypeError("can't assign sequence to element index")
-                if not hasattr(value, "__index__"):
-                    raise TypeError(_BAD_VALUE_TYPE)
-                self._data[self._offset + key * self._strides[0]] = int(
-                    value  # type: ignore[arg-type]
-                )
-            elif hasattr(value, "__len__"):
                 raise NotImplementedError  # TODO
             else:
                 if not hasattr(value, "__index__"):
                     raise TypeError(_BAD_VALUE_TYPE)
                 offset = self._offset + key * self._strides[0]
-                for i in range(offset, offset + self._shape[1], self._strides[1]):
-                    self._data[i] = int(value)  # type: ignore[arg-type]
+                if self._ndim == 1:
+                    self._data[offset] = int(value)  # type: ignore[arg-type]
+                else:
+                    for i in range(offset, offset + self._shape[1], self._strides[1]):
+                        self._data[i] = int(value)  # type: ignore[arg-type]
 
         elif isinstance(key, slice):
-            raise NotImplementedError  # TODO
+            if hasattr(value, "__len__"):
+                raise NotImplementedError  # TODO
+            else:
+                if not hasattr(value, "__index__"):
+                    raise TypeError(_BAD_VALUE_TYPE)
+
+                for start, stop, step in self._contiguous_blocks(key):
+                    for i in range(start, stop, step):
+                        self._data[i] = int(value)  # type: ignore[arg-type]
 
         elif isinstance(key, tuple):
             a, b = key
