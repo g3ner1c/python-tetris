bad34b7489b1153d9640686c0f621ab7c69b6bae
Sofia N. Lima <me@dzshn.xyz>
2023-01-18 12:57:07 -0300

    fix: back-to-back not working for t-spins

--- tetris/impl/scorer.py
+++ tetris/impl/scorer.py
@@ -37,6 +37,51 @@ class GuidelineScorer(Scorer):
         piece = delta.game.piece
         board = delta.game.board
 
+        self.tspin = False
+        self.tspin_mini = False
+
+        # useful: https://tetris.wiki/T-Spin#Current_rules
+        if delta.kind == MoveKind.ROTATE and piece.type == PieceType.T and delta.r != 0:
+            px = piece.x
+            py = piece.y
+            corners = []
+            # check corners clockwise from top-left
+            for x, y in [(0, 0), (0, 2), (2, 2), (2, 0)]:
+                corners.append(
+                    x + px not in range(board.shape[0])
+                    or y + py not in range(board.shape[1])
+                    or board[x + px, y + py] != 0
+                )
+
+            back = None
+            # find the back of the piece clockwise from top. note how this
+            # is checked in the same order as the corners: corners[back] will
+            # be the corner before the back edge (behind counter-clockwise)
+            for i, pos in enumerate([(0, 1), (1, 2), (2, 1), (1, 0)]):
+                if pos not in piece.minos:
+                    back = i
+                    break
+
+            # ideally, an edge is always found. unless:
+            #  - the piece's center is not 1,1 (should this be checked?)
+            #  - its not T-shaped
+            if back is not None:
+                front_corners = corners[(back + 2) % 4] + corners[(back + 3) % 4]
+                back_corners = corners[(back + 0) % 4] + corners[(back + 1) % 4]
+                # if there are two corners in the front edge..
+                if front_corners == 2 and back_corners >= 1:
+                    # it's a proper t-spin!
+                    self.tspin = True
+                # but, if there is one corner in front edge and two in the back..
+                elif front_corners == 1 and back_corners == 2:
+                    # this is still a tspin!
+                    if abs(delta.x) == 2 and abs(delta.y) == 1:
+                        # the piece was kicked far, proper t-spin!
+                        self.tspin = True
+                    else:
+                        # last case, mini-tspin
+                        self.tspin_mini = True
+
         if delta.kind == MoveKind.SOFT_DROP:  # soft drop
             if not delta.auto:
                 self.score += delta.x
@@ -89,51 +134,6 @@ class GuidelineScorer(Scorer):
             if self.line_clears >= self.goal:
                 self.goal += 10
                 self.level += 1
-
-        self.tspin = False
-        self.tspin_mini = False
-
-        # useful: https://tetris.wiki/T-Spin#Current_rules
-        if delta.kind == MoveKind.ROTATE and piece.type == PieceType.T and delta.r != 0:
-            px = piece.x
-            py = piece.y
-            corners = []
-            # check corners clockwise from top-left
-            for x, y in [(0, 0), (0, 2), (2, 2), (2, 0)]:
-                corners.append(
-                    x + px not in range(board.shape[0])
-                    or y + py not in range(board.shape[1])
-                    or board[x + px, y + py] != 0
-                )
-
-            back = None
-            # find the back of the piece clockwise from top. note how this
-            # is checked in the same order as the corners: corners[back] will
-            # be the corner before the back edge (behind counter-clockwise)
-            for i, pos in enumerate([(0, 1), (1, 2), (2, 1), (1, 0)]):
-                if pos not in piece.minos:
-                    back = i
-                    break
-
-            # ideally, an edge is always found. unless:
-            #  - the piece's center is not 1,1 (should this be checked?)
-            #  - its not T-shaped
-            if back is not None:
-                front_corners = corners[(back + 2) % 4] + corners[(back + 3) % 4]
-                back_corners = corners[(back + 0) % 4] + corners[(back + 1) % 4]
-                # if there are two corners in the front edge..
-                if front_corners == 2 and back_corners >= 1:
-                    # it's a proper t-spin!
-                    self.tspin = True
-                # but, if there is one corner in front edge and two in the back..
-                elif front_corners == 1 and back_corners == 2:
-                    # this is still a tspin!
-                    if abs(delta.x) == 2 and abs(delta.y) == 1:
-                        # the piece was kicked far, proper t-spin!
-                        self.tspin = True
-                    else:
-                        # last case, mini-tspin
-                        self.tspin_mini = True
 
 
 class NESScorer(Scorer):
